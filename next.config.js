1eaa9d4c975ea08328813cd7ecc73e15f2cb4430
Kotov <kotov.smtp@gmail.com>
2023-04-23 15:17:55 +0300

    ⚙️ env moved

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	env: {
-		API_URL: 'https://peaceful-citadel-43962.herokuapp.com',
-		TO_EMAIL: 'kotov.smtp@gmail.com',
-	},
+  env: {
+    API_URL: process.env.API_URL || 'https://localhost:1337',
+    TO_EMAIL: process.env.TO_EMAIL
+  }
 };
